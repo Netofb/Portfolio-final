a5e56a33fedc25f6099a5d6c8286f84271438486
Fabio Barros lima <106633969+Netofb@users.noreply.github.com>
2023-09-30 14:18:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("tochstart", toggleMenu);
+
+
+
+function initAnimacaoScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length){
+    const windowMetade = window.innerHeight * 0.7;
+      function animaScroll(){
+        sections.forEach((section)=>{
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - windowMetade) < 0;
+            if(isSectionVisible){
+              section.classList.add('ativo');
+            }else{
+              section.classList.remove('ativo');
+            }
+        })
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
+
+
