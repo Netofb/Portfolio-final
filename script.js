c8819091e3b49b53e92511284b5f9a8200c0a84d
Fabio Barros lima <fabi.neto.fb@gmail.com>
2025-06-19 18:02:36 -0300

    reformando

--- script.js
+++ script.js
@@ -10,20 +10,20 @@ btnMobile.addEventListener("tochstart", toggleMenu);
 
 
 
-function initAnimacaoScroll(){
+function initAnimacaoScroll() {
   const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length){
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.7;
-      function animaScroll(){
-        sections.forEach((section)=>{
-            const sectionTop = section.getBoundingClientRect().top;
-            const isSectionVisible = (sectionTop - windowMetade) < 0;
-            if(isSectionVisible){
-              section.classList.add('ativo');
-            }else{
-              section.classList.remove('ativo');
-            }
-        })
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      })
     }
     animaScroll();
     window.addEventListener('scroll', animaScroll);
